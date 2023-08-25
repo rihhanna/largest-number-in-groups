aa281d87d0548b953f8d79ce40170ca009eaf499
rihhana <hrihhana@gmail.com>
2023-08-25 19:12:47 +0300

    done it

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Good Luck 💪🏾
+function findLargestNumbers(arrays) {
+    // create an empty array to store the largest numbers
+    let largest = [];
+    // loop through the arrays
+    for (let array of arrays) {
+      // find the maximum number in the array
+      let max = Math.max(...array);
+      // push it to the largest array
+      largest.push(max);
+    }
+    // return the largest array
+    return largest;
+  }
+console.log(findLargestNumbers([[7, 3, 8, 11], [54, 12, 43, 77], [16, 11, 8]]))  
+console.log(findLargestNumbers([[22, 55, 12], [45, 66, 95], [22, 5, 11]])) //➞ [55, 95, 22])
